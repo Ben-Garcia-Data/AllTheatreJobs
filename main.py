62c4247bbf92b908a3fa611bd0c8e0a2fb2cbc02
ThechnicallyAnIdiot <ben@garciafamily.co.uk>
2021-07-31 01:23:40 +0100

    Version 0.21 Release (Stable?) but for realisies this time

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def Web_Scraping():
     def SetDriverOptions():
         userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
         chrome_options = ChromeOptions()
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument(f'--user-agent={userAgent}')
